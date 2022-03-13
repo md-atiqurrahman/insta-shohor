8d48f02d3898c6c2a96698ecc7a06d816afc0301
md-atiqurrahman <mdatiqurrahman9104@gmail.com>
2022-03-13 20:51:08 +0600

    fix heart icon content number

--- js/app.js
+++ js/app.js
@@ -34,17 +34,21 @@ const switchTab = (id) => {
     if (id === "posts") {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById("liked").textContent = '';
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById("reported").textContent = '';
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
+        document.getElementById("reported").textContent = '';
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
+        document.getElementById("liked").textContent = '';
 
         displayReportedPosts();
     }
